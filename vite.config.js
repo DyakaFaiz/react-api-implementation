26c60c00ed0fbf413c62e3acf635d9c487e87e2d
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-01-06 13:23:45 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://demo-api.syaifur.io', 
+        target: 'http://demo-api.syaifur.io', // Base URL API Anda
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''), // Hapus prefix '/api' sebelum mengirimkan ke server
       },
     },
   },
